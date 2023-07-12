fac3ebd1f758239e89cffd4eed3b9e2fb08df5f1
Fajar Januriawan <januriawan@users.noreply.github.com>
2023-07-12 14:51:40 +0700

    Update Layout
    Just Updating New Layout

--- januriawan/about/js/index.js
+++
@@ -1,7 +0,0 @@
-var buttons = $('.main-nav__item');
-buttons.bind('click', function(){
-   if(!$(this).hasClass('active')) {
-       $('.main-nav__item.active').removeClass('active');
-       $(this).addClass('active');
-   }
-});
